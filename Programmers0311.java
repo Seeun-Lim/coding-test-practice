820f0e4fcd9842afd0b89ad79ac5e57aab0bff85
LIM SEEUN <ssen.lim@gmail.com>
2023-03-11 05:52:52 +0900

    [프로그래머스] Lv2 - 주식가격(1)

---
+++ Programmers0311.java
@@ -0,0 +1,16 @@
+public class Programmers0311 {
+    public int[] solution(int[] prices) {
+        int[] ans = new int[prices.length];
+
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                ans[i]++;
+                if (prices[i] > prices[j])
+                    break;
+
+            }
+        }
+
+        return ans;
+    }
+}
